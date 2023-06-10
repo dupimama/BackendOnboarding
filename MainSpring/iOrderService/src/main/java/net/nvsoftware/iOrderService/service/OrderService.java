739d3c37ac58e16295fb4b6cefbb0adc5142f667
dupimama <92074050+dupimama@users.noreply.github.com>
2023-06-09 17:04:43 -0700

    Delete MainSpring directory

--- MainSpring/iOrderService/src/main/java/net/nvsoftware/iOrderService/service/OrderService.java
+++
@@ -1,7 +0,0 @@
-package net.nvsoftware.iOrderService.service;
-
-import net.nvsoftware.iOrderService.model.OrderRequest;
-
-public interface OrderService {
-    long placeOrder(OrderRequest orderRequest);
-}
--- MainSpring/iProductService/src/main/java/net/nvsoftware/iProductService/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package net.nvsoftware.iProductService.repository;
-
-import net.nvsoftware.iProductService.entity.ProductEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//JpaRepository<whats in a JPA, ID(in generic form"Long">
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-}
